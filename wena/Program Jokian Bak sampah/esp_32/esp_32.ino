9cd91dfae384b802dbcb7f8520bd7db82794523b
Shalahuddin Yoga Wardana Abdullah <70340926+WardanaYoga@users.noreply.github.com>
2026-03-17 21:57:39 +0700

    Add files via upload

---
+++ wena/Program Jokian Bak sampah/esp_32/esp_32.ino
@@ -0,0 +1,49 @@
+#include <BluetoothSerial.h>
+#include <ESP32Servo.h>
+
+BluetoothSerial SerialBT;
+
+Servo servoOrganik;
+Servo servoAnorganik;
+
+void setup() {
+
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_SAMPAH");
+
+  servoOrganik.attach(18);
+  servoAnorganik.attach(19);
+
+}
+
+void loop() {
+
+  if (SerialBT.available()) {
+
+    char cmd = SerialBT.read();  // baca 1 karakter
+
+    if (cmd == '0') {  // organik
+
+      servoOrganik.write(90);
+      delay(3000);
+      servoOrganik.write(0);
+
+    }
+
+    else if (cmd == '1') {  // anorganik
+
+      servoAnorganik.write(90);
+      delay(3000);
+      servoAnorganik.write(0);
+
+    }
+
+    else if (cmd == '2'){
+      servoOrganik.write(0);
+      servoAnorganik.write(0);
+      delay(3000);
+    }
+
+  }
+
+}
